817fd6ca11f666fb273bbf36b57bece4e0463898
Marcia da Silva <marcia.dasilva@unijui.edu.br>
2017-09-06 16:57:56 -0300

    layout da pagina do produto

--- view/shop.php
+++ view/shop.php
@@ -2,158 +2,32 @@
 
 <section>
 
-    <div class="container" id="destaque-produtos-container" ng-controller="destaque-controller">
-
-        <div id="destaque-produtos">
-
-            <div class="item" ng-repeat="produto in produtos">
-
-                <div class="col-sm-6 col-imagem">
-                    <img src="img/produtos/{{produto.foto_principal}}" alt="{{produto.nome_prod_longo}}">
+    <div class="container" id="produto-selecionado">
+        
+        <div id="detalhes-produto-selecionado">
+            <div class="item">
+                <div class="col-sm-8 col-imagem">
+                    <img src="img/produtos/produtos1.jpg">
                 </div>
-                <div class="col-sm-6 col-descricao">
-                    <h2>{{produto.nome_prod_longo}}</h2>
+                <div class="col-sm-4 col-descricao">
+                    <h2>Vestido GG de Viscose</h2>
                     <div class="box-valor">
-                        <div class="text-noboleto text-arial-cinza">no boleto</div>
+                        <div class="text-avista text-arial-cinza">no boleto</div>
                         <div class="text-por text-arial-cinza">por</div>
-                        <div class="text-reais text-roxo">R$</div>
-                        <div class="text-valor text-roxo">{{produto.preco}}</div>
-                        <div class="text-valor-centavos text-roxo">,{{produto.centavos}}</div>
-                        <div class="text-parcelas text-arial-cinza">ou em até {{produto.parcelas}}x de R$ {{produto.parcela}}</div>
-                        <div class="text-total text-arial-cinza">total a prazo R$ {{produto.total}}</div>
+                        <div class="text-reais text-arial-rosa">R$</div>
+                        <div class="text-valor text-arial-rosa">1.259</div>
+                        <div class="text-valor-centavos text-arial-rosa">, 50</div>
+                        <div class="text-parcelas text-arial-cinza">ou em até 4x de R$ 31,21</div>
+                        <div class="text-total text-arial-cinza">total a prazo R$ 124,84</div>
                     </div>
-                    <a href="#" class="btn btn-comprar text-roxo"><i class="fa fa-shopping-cart"></i> compre agora</a>
+                    <a href="#" class="btn btn-comprar text-arial-rosa"><i class="fa fa-shopping-bag"></i> Compre agora</a>
                 </div>
-
             </div>
-
+            
         </div>
-
-        <button type="button" id="btn-destaque-prev"><i class="fa fa-angle-left"></i></button>
-        <button type="button" id="btn-destaque-next"><i class="fa fa-angle-right"></i></button>
-
+        
     </div>
 
-    <div id="promocoes" class="container">
-
-        <div class="row">
-            <div class="col-md-2">
-
-                <div class="box-promocao box-1">
-                    <p>escolha por desconto</p>
-                </div>
-
-            </div>
-            <div class="col-md-10">
-
-                <div class="row-fluid">
-                    <div class="col-md-3">
-                        <div class="box-promocao">
-                            <div class="text-ate">até</div>
-                            <div class="text-numero">40</div>
-                            <div class="text-porcento">%</div>
-                            <div class="text-off">off</div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="box-promocao">
-                            <div class="text-ate">até</div>
-                            <div class="text-numero">60</div>
-                            <div class="text-porcento">%</div>
-                            <div class="text-off">off</div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="box-promocao">
-                            <div class="text-ate">até</div>
-                            <div class="text-numero">80</div>
-                            <div class="text-porcento">%</div>
-                            <div class="text-off">off</div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="box-promocao">
-                            <div class="text-ate">até</div>
-                            <div class="text-numero">85</div>
-                            <div class="text-porcento">%</div>
-                            <div class="text-off">off</div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-    </div>
-
-    <div id="mais-buscados" class="container">
-
-        <div class="row text-center title-default-roxo">
-            <h2>os mais buscados</h2>
-            <hr>
-        </div>
-
-        <div class="row">
-
-            <div class="col-md-3">
-                <div class="box-produto-info">
-                    <a href="#">
-                        <img src="img/produtos/panelas.png" alt="Panelas" class="produto-img">
-                        <h3>Conjunto de Panelas Tramontina Versalhes Alumínio Antiaderente 5</h3>
-                        <div class="estrelas" data-score="3"></div>
-                        <div class="text-qtd-reviews text-arial-cinza">(300)</div>
-                        <div class="text-valor text-roxo">R$ 109,90</div>
-                        <div class="text-parcelado text-arial-cinza">10x de R$ 10,99 sem juros</div>
-                    </a>
-                </div>
-
-            </div>
-
-            <div class="col-md-3">
-                <div class="box-produto-info">
-                    <a href="#">
-                        <img src="img/produtos/panelas.png" alt="Panelas" class="produto-img">
-                        <h3>Conjunto de Panelas Tramontina Versalhes Alumínio Antiaderente 5</h3>
-                        <div class="estrelas"  data-score="5"></div>
-                        <div class="text-qtd-reviews text-arial-cinza">(300)</div>
-                        <div class="text-valor text-roxo">R$ 109,90</div>
-                        <div class="text-parcelado text-arial-cinza">10x de R$ 10,99 sem juros</div>
-                    </a>
-                </div>
-
-            </div>
-
-            <div class="col-md-3">
-                <div class="box-produto-info">
-                    <a href="#">
-                        <img src="img/produtos/panelas.png" alt="Panelas" class="produto-img">
-                        <h3>Conjunto de Panelas Tramontina Versalhes Alumínio Antiaderente 5</h3>
-                        <div class="estrelas"  data-score="2.5"></div>
-                        <div class="text-qtd-reviews text-arial-cinza">(300)</div>
-                        <div class="text-valor text-roxo">R$ 109,90</div>
-                        <div class="text-parcelado text-arial-cinza">10x de R$ 10,99 sem juros</div>
-                    </a>
-                </div>
-
-            </div>
-
-            <div class="col-md-3">
-                <div class="box-produto-info">
-                    <a href="#">
-                        <img src="img/produtos/panelas.png" alt="Panelas" class="produto-img">
-                        <h3>Conjunto de Panelas Tramontina Versalhes Alumínio Antiaderente 5</h3>
-                        <div class="estrelas"  data-score="5"></div>
-                        <div class="text-qtd-reviews text-arial-cinza">(300)</div>
-                        <div class="text-valor text-roxo">R$ 109,90</div>
-                        <div class="text-parcelado text-arial-cinza">10x de R$ 10,99 sem juros</div>
-                    </a>
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
 
 </section>
 
